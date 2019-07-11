cb32266a71441a58c5dde2ae21b1503e1e674ea6
NickSagalbaev <NickSagalbaev@gmail.com>
2019-07-11 21:09:21 +0600

    Реализовал класс AstraPlatCard, добавил нужные методы.

--- AstraPlat/AstraPlat/AstraPlatCard.cs
+++ AstraPlat/AstraPlat/AstraPlatCard.cs
@@ -8,6 +8,42 @@ namespace AstraPlat
 {
     class AstraPlatCard
     {
+        private int wallet;
+        private int id;
+        private static int numberForID = 111;
 
+        public AstraPlatCard()
+        {
+            id = numberForID++;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+        public int GetWallet()
+        {
+            return wallet;
+        }
+
+        public bool Valid()
+        {
+            if (wallet > 90)
+            {
+                wallet -= 90;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Refill(int cash)
+        {
+            if (cash > 0 && cash <= 10000)
+            {
+                wallet += cash;
+                return true;
+            }
+            return false;
+        }
     }
 }
