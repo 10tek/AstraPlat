2948b825da24b0b6f3f46e3e64b028919e8adc92
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-07-11 21:13:17 +0600

    Написал меню и реализовал пункт покупки карты и выход.

--- AstraPlat/AstraPlat/Program.cs
+++ AstraPlat/AstraPlat/Program.cs
@@ -10,6 +10,30 @@ namespace AstraPlat
     {
         static void Main(string[] args)
         {
+            List<AstraPlatCard> cards = new List<AstraPlatCard>();
+            bool isExit = false;
+            int menu;
+            while (!isExit)
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Купить карту");
+                Console.WriteLine("2 - Валидация");
+                Console.WriteLine("3 - Пополнить счёт");
+                Console.WriteLine("0 - Выход");
+                menu = Convert.ToInt32(Console.ReadLine());
+                switch (menu)
+                {
+                    case 0: isExit = true; break;
+                    case 1:
+                        AstraPlatCard tmp = new AstraPlatCard();
+                        Console.Clear();
+                        Console.WriteLine($"Номер вашей карточки: {tmp.GetId()}");
+                        cards.Add(tmp);
+                        Console.Write("Нажмите любую клавишу что бы продолжить");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
     }
 }
